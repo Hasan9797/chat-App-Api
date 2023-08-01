7dc0283e73a3c209f2c2ae64e77d260651d42247
Hasan <hasanjonsadullayev97@gmail.com>
2023-08-01 17:02:49 +0500

    update full file

--- src/middleware/errorHandling.js
+++
@@ -1,9 +0,0 @@
-export default (error, req, res) => {
-	if (error.isError) {
-		res
-			.status(error.statusCode)
-			.json({ name: error.name, message: error.message });
-	}
-
-	res.status(error.statusCode || 500).json({ message: error.message });
-};
